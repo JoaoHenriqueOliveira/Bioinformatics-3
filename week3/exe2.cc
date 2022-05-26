72402be6679f6d9d9eb6495c160ed5d32aca8f88
JoaoHenriqueOliveira <joaojhro@gmail.com>
2022-05-26 17:00:11 +0200

    exe2 corrected, good logic and edge cases

--- week3/exe2.cc
+++ week3/exe2.cc
@@ -18,11 +18,10 @@ void populate(string file, string &v, string &w, int &match, int &mu, int &sigma
 int main(int argc, char const *argv[])
 {
     string file_name = argv[1], v, w;
-    int match, mu, sigma, middle_index; // match reward, mismatch penalty, indel penalty
-
+    int match, mu, sigma, middle_index;
     populate(file_name, v, w, match, mu, sigma);
 
-    middle_index = w.size() / 2; // w.size() % 2 == 0 ? w.size() / 2 : 1 + w.size() / 2;
+    middle_index = w.size() / 2;
 
     auto edges = MiddleEdge(v, w, middle_index, match, mu, sigma);
 
@@ -32,69 +31,158 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-void initialize_score(vector<vector<int>> &score, int n, int indel)
+void initialize_score(vector<vector<int>> &from_src, vector<vector<int>> &to_sink, int n, int indel)
 {
     vector<int> v1(n, 0), v2(n, 0);
-    score.push_back(v1);
-    score.push_back(v2);
+    from_src.push_back(v1);
+    from_src.push_back(v2);
 
-    score[1][0] = -indel;
+    to_sink.push_back(v1);
+    to_sink.push_back(v2);
+
+    from_src[1][0] = -indel;
+    to_sink[1][0] = -indel;
 
     for (int i = 1; i < n; i++)
     {
-        score[0][i] -= i * indel;
+        from_src[0][i] -= i * indel;
+        to_sink[0][i] -= i * indel;
     }
+
+    return;
+}
+
+void build_length_i(vector<vector<int>> &from_src, vector<vector<int>> &to_sink, int curr_index, int next_index, int n, int m, vector<vector<int>> &length_i)
+{
+    vector<int> v1, v2;
+    int src_node, sink_node;
+    if (m % 2 == 0)
+    {
+        for (int i = 0; i <= n; i++)
+        {
+            src_node = from_src[curr_index][i] + to_sink[curr_index][n - i];
+            v1.push_back(src_node);
+        }
+
+        length_i.push_back(v1);
+
+        for (int i = 0; i <= n; i++)
+        {
+            sink_node = from_src[next_index][i] + to_sink[next_index][n - i];
+            v2.push_back(sink_node);
+        }
+
+        length_i.push_back(v2);
+    }
+    else
+    {
+        for (int i = 0; i <= n; i++)
+        {
+            src_node = from_src[curr_index][i] + to_sink[next_index][n - i];
+            v1.push_back(src_node);
+        }
+
+        length_i.push_back(v1);
+
+        for (int i = 0; i <= n; i++)
+        {
+            sink_node = from_src[next_index][i] + to_sink[curr_index][n - i];
+            v2.push_back(sink_node);
+        }
+
+        length_i.push_back(v2);
+    }
+    return;
+}
+
+void display(vector<vector<int>> &v, int curr, int next)
+{
+
+    for (int j = 0; j < v[0].size(); j++)
+    {
+        cout << " " << v[curr][j] << '\t' << v[next][j] << '\n';
+    }
+    cout << '\n';
 
     return;
 }
 
 vector<vector<int>> MiddleEdge(string &v, string &w, int middle_index, int &match, int &mu, int &indel)
 {
-    int n = v.size(), m = w.size(), match_score(0), curr_index(0), max_index(0);
-    vector<vector<int>> score;
+    int n = v.size(), m = w.size(), match_score_src(0), match_score_sink(0), curr_index(0), middle_node_src(0), middle_node_sink(0);
+    vector<vector<int>> from_src, to_sink, length_i;
 
-    initialize_score(score, n + 1, indel);
+    initialize_score(from_src, to_sink, n + 1, indel);
 
     for (int j = 1; j <= middle_index; j++)
     {
         curr_index = j % 2;
-        score[curr_index][0] = -j * indel;
+        from_src[curr_index][0] = -j * indel;
+        to_sink[curr_index][0] = -j * indel;
 
         for (int i = 1; i <= n; i++)
         {
-            match_score = v[i - 1] == w[j - 1] ? match : -mu;
+            match_score_src = v[i - 1] == w[j - 1] ? match : -mu;
+            match_score_sink = v[n - i] == w[m - j] ? match : -mu;
 
-            score[curr_index][i] = max(max(score[abs(curr_index - 1)][i] - indel, score[curr_index][i - 1] - indel),
-                                       score[abs(curr_index - 1)][i - 1] + match_score);
+            from_src[curr_index][i] = max(max(from_src[abs(curr_index - 1)][i] - indel, from_src[curr_index][i - 1] - indel),
+                                          from_src[abs(curr_index - 1)][i - 1] + match_score_src);
+            to_sink[curr_index][i] = max(max(to_sink[abs(curr_index - 1)][i] - indel, to_sink[curr_index][i - 1] - indel),
+                                         to_sink[abs(curr_index - 1)][i - 1] + match_score_sink);
         }
     }
 
-    max_index = max_element(score[curr_index].begin(), score[curr_index].end()) - score[curr_index].begin(); // first max argval of vector
-
     int next_index = abs(curr_index - 1);
 
-    score[next_index][0] = -(middle_index + 1) * indel;
-
-    for (int i = 1; i <= n; i++)
+    // Align the values of distances accordingly to parity of input string
+    if (m % 2 == 0)
     {
-        match_score = v[i - 1] == w[middle_index] ? match : -mu;
-
-        score[next_index][i] = max(max(score[curr_index][i] - indel, score[next_index][i - 1] - indel),
-                                   score[curr_index][i - 1] + match_score);
-    }
-
-    int diag_score = score[next_index][max_index + 1], horz_score = score[next_index][max_index], vertical_score = score[curr_index][max_index + 1];
-
-    if (vertical_score >= diag_score && vertical_score >= horz_score)
-    {
-        return {{max_index, middle_index}, {max_index + 1, middle_index}};
-    }
-    else if (horz_score >= diag_score && horz_score >= vertical_score)
-    {
-        return {{max_index, middle_index}, {max_index, middle_index + 1}};
+        from_src[next_index][0] = -(middle_index + 1) * indel;
+        for (int i = 1; i <= n; i++)
+        {
+            match_score_src = v[i - 1] == w[middle_index] ? match : -mu;
+            from_src[next_index][i] = max(max(from_src[curr_index][i] - indel, from_src[next_index][i - 1] - indel),
+                                          from_src[curr_index][i - 1] + match_score_src);
+        }
     }
     else
     {
-        return {{max_index, middle_index}, {max_index + 1, middle_index + 1}};
+        from_src[next_index][0] = -(middle_index + 1) * indel;
+        to_sink[next_index][0] = -(middle_index + 1) * indel;
+        for (int i = 1; i <= n; i++)
+        {
+            match_score_src = v[i - 1] == w[middle_index] ? match : -mu;
+            match_score_sink = v[n - i] == w[middle_index] ? match : -mu;
+
+            from_src[next_index][i] = max(max(from_src[curr_index][i] - indel, from_src[next_index][i - 1] - indel),
+                                          from_src[curr_index][i - 1] + match_score_src);
+            to_sink[next_index][i] = max(max(to_sink[curr_index][i] - indel, to_sink[next_index][i - 1] - indel),
+                                         to_sink[curr_index][i - 1] + match_score_sink);
+        }
+    }
+
+    build_length_i(from_src, to_sink, curr_index, next_index, n, m, length_i);
+
+    // display(from_src, curr_index, next_index);
+    // display(to_sink, curr_index, next_index);
+    // display(length_i, 0, 1);
+
+    middle_node_src = max_element(length_i[0].begin(), length_i[0].end()) - length_i[0].begin();
+    middle_node_sink = max_element(length_i[1].begin(), length_i[1].end()) - length_i[1].begin();
+
+    if (middle_node_src == middle_node_sink)
+    {
+        // cout << "Horz" << '\n';
+        return {{middle_node_src, middle_index}, {middle_node_src, middle_index + 1}};
+    }
+    else if (middle_node_src + 1 == middle_node_sink)
+    {
+        // cout << "Diag" << '\n';
+        return {{middle_node_src, middle_index}, {middle_node_sink, middle_index + 1}};
+    }
+    else
+    {
+        // cout << "Vertical" << '\n';
+        return {{middle_node_src, middle_index}, {middle_node_src + 1, middle_index}};
     }
 }
