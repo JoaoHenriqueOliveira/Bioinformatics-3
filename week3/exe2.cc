6e0bce9382d749595915c12091a1ba05aa5a345b
JoaoHenriqueOliveira <joaojhro@gmail.com>
2022-05-28 19:28:56 +0200

    Re-write exe2 solution to a more clean solution (more modules)

--- week3/exe2.cc
+++ week3/exe2.cc
@@ -23,10 +23,10 @@ int main(int argc, char const *argv[])
 
     middle_index = w.size() / 2;
 
-    auto edges = MiddleEdge(v, w, middle_index, match, mu, sigma);
+    auto edge = MiddleEdge(v, w, middle_index, match, mu, sigma);
 
-    cout << edges[0][0] << " " << edges[0][1] << '\n'
-         << edges[1][0] << ' ' << edges[1][1] << '\n';
+    cout << edge[0][0] << ' ' << edge[0][1] << '\n'
+         << edge[1][0] << ' ' << edge[1][1] << '\n';
 
     return 0;
 }
@@ -52,137 +52,93 @@ void initialize_score(vector<vector<int>> &from_src, vector<vector<int>> &to_sin
     return;
 }
 
-void build_length_i(vector<vector<int>> &from_src, vector<vector<int>> &to_sink, int curr_index, int next_index, int n, int m, vector<vector<int>> &length_i)
+void FromSrc(string &v, string &w, vector<vector<int>> &from_src, int col, int &match, int &mu, int &indel)
 {
-    vector<int> v1, v2;
-    int src_node, sink_node;
-    if (m % 2 == 0)
+    int curr_col, prev_col, n = v.size(), m = w.size(), match_score;
+
+    assert(col >= 0 && col <= m);
+
+    for (int j = 1; j <= col; j++)
     {
-        for (int i = 0; i <= n; i++)
+        curr_col = j % 2;
+        prev_col = abs(curr_col - 1);
+        from_src[curr_col][0] = -j * indel;
+
+        for (int i = 1; i <= n; i++)
         {
-            src_node = from_src[curr_index][i] + to_sink[curr_index][n - i];
-            v1.push_back(src_node);
+            match_score = v[i - 1] == w[j - 1] ? match : -mu;
+            from_src[curr_col][i] = max(max(from_src[prev_col][i] - indel, from_src[curr_col][i - 1]),
+                                        from_src[prev_col][i - 1] + match_score);
         }
-
-        length_i.push_back(v1);
-
-        for (int i = 0; i <= n; i++)
-        {
-            sink_node = from_src[next_index][i] + to_sink[next_index][n - i];
-            v2.push_back(sink_node);
-        }
-
-        length_i.push_back(v2);
     }
-    else
-    {
-        for (int i = 0; i <= n; i++)
-        {
-            src_node = from_src[curr_index][i] + to_sink[next_index][n - i];
-            v1.push_back(src_node);
-        }
-
-        length_i.push_back(v1);
-
-        for (int i = 0; i <= n; i++)
-        {
-            sink_node = from_src[next_index][i] + to_sink[curr_index][n - i];
-            v2.push_back(sink_node);
-        }
-
-        length_i.push_back(v2);
-    }
-    return;
-}
-
-void display(vector<vector<int>> &v, int curr, int next)
-{
-
-    for (int j = 0; j < v[0].size(); j++)
-    {
-        cout << " " << v[curr][j] << '\t' << v[next][j] << '\n';
-    }
-    cout << '\n';
 
     return;
 }
 
-vector<vector<int>> MiddleEdge(string &v, string &w, int middle_index, int &match, int &mu, int &indel)
+void ToSink(string &v, string &w, vector<vector<int>> &to_sink, int col, int &match, int &mu, int &indel)
 {
-    int n = v.size(), m = w.size(), match_score_src(0), match_score_sink(0), curr_index(0), middle_node_src(0), middle_node_sink(0);
-    vector<vector<int>> from_src, to_sink, length_i;
+    int curr_col, prev_col, n = v.size(), m = w.size(), match_score;
+    assert(col >= 0 && col <= m);
 
+    for (int j = 1; j <= m - col; j++)
+    {
+        curr_col = j % 2;
+        prev_col = abs(curr_col - 1);
+        to_sink[curr_col][0] = -j * indel;
+
+        for (int i = 1; i <= n; i++)
+        {
+            match_score = v[n - i] == w[m - j] ? match : -mu;
+            to_sink[curr_col][i] = max(max(to_sink[prev_col][i] - indel, to_sink[curr_col][i - 1]),
+                                       to_sink[prev_col][i - 1] + match_score);
+        }
+    }
+
+    return;
+}
+
+vector<int> MiddleColumn(string &v, string &w, int col, int &match, int &mu, int &indel)
+{
+    int n = v.size(), m = w.size();
+    vector<int> length_i(n + 1, 0);
+    vector<vector<int>> from_src, to_sink;
     initialize_score(from_src, to_sink, n + 1, indel);
 
-    for (int j = 1; j <= middle_index; j++)
+    FromSrc(v, w, from_src, col, match, mu, indel); // from_src[col % 2] -> middle column
+    ToSink(v, w, to_sink, col, match, mu, indel);   // to_sink[(m - col) % 2] -> middle column
+
+    for (int i = 0; i <= n; i++)
     {
-        curr_index = j % 2;
-        from_src[curr_index][0] = -j * indel;
-        to_sink[curr_index][0] = -j * indel;
-
-        for (int i = 1; i <= n; i++)
-        {
-            match_score_src = v[i - 1] == w[j - 1] ? match : -mu;
-            match_score_sink = v[n - i] == w[m - j] ? match : -mu;
-
-            from_src[curr_index][i] = max(max(from_src[abs(curr_index - 1)][i] - indel, from_src[curr_index][i - 1] - indel),
-                                          from_src[abs(curr_index - 1)][i - 1] + match_score_src);
-            to_sink[curr_index][i] = max(max(to_sink[abs(curr_index - 1)][i] - indel, to_sink[curr_index][i - 1] - indel),
-                                         to_sink[abs(curr_index - 1)][i - 1] + match_score_sink);
-        }
+        length_i[i] = from_src[col % 2][i] + to_sink[(m - col) % 2][n - i];
     }
 
-    int next_index = abs(curr_index - 1);
+    return length_i;
+}
 
-    // Align the values of distances accordingly to parity of input string
-    if (m % 2 == 0)
+vector<vector<int>> MiddleEdge(string &v, string &w, int col, int &match, int &mu, int &indel)
+{
+    int n = v.size(), m = w.size(), match_score_src(0), match_score_sink(0), curr_index(0), middle_node_src(0), middle_node_sink(0);
+
+    assert(col < m);
+
+    vector<int> middle_col_src = MiddleColumn(v, w, col, match, mu, indel),
+                middle_col_tgt = MiddleColumn(v, w, col + 1, match, mu, indel);
+
+    int middle_node = max_element(middle_col_src.begin(), middle_col_src.end()) - middle_col_src.begin();
+
+    if (middle_col_src[middle_node] == middle_col_tgt[middle_node + 1])
     {
-        from_src[next_index][0] = -(middle_index + 1) * indel;
-        for (int i = 1; i <= n; i++)
-        {
-            match_score_src = v[i - 1] == w[middle_index] ? match : -mu;
-            from_src[next_index][i] = max(max(from_src[curr_index][i] - indel, from_src[next_index][i - 1] - indel),
-                                          from_src[curr_index][i - 1] + match_score_src);
-        }
+        // Diag edge
+        return {{middle_node, col}, {middle_node + 1, col + 1}};
     }
-    else
+    else if (middle_col_src[middle_node] == middle_col_tgt[middle_node])
     {
-        from_src[next_index][0] = -(middle_index + 1) * indel;
-        to_sink[next_index][0] = -(middle_index + 1) * indel;
-        for (int i = 1; i <= n; i++)
-        {
-            match_score_src = v[i - 1] == w[middle_index] ? match : -mu;
-            match_score_sink = v[n - i] == w[middle_index] ? match : -mu;
-
-            from_src[next_index][i] = max(max(from_src[curr_index][i] - indel, from_src[next_index][i - 1] - indel),
-                                          from_src[curr_index][i - 1] + match_score_src);
-            to_sink[next_index][i] = max(max(to_sink[curr_index][i] - indel, to_sink[next_index][i - 1] - indel),
-                                         to_sink[curr_index][i - 1] + match_score_sink);
-        }
-    }
-
-    build_length_i(from_src, to_sink, curr_index, next_index, n, m, length_i);
-
-    // display(from_src, curr_index, next_index);
-    // display(to_sink, curr_index, next_index);
-    // display(length_i, 0, 1);
-
-    middle_node_src = max_element(length_i[0].begin(), length_i[0].end()) - length_i[0].begin();
-    middle_node_sink = max_element(length_i[1].begin(), length_i[1].end()) - length_i[1].begin();
-
-    if (middle_node_src == middle_node_sink)
-    {
-        // cout << "Horz" << '\n';
-        return {{middle_node_src, middle_index}, {middle_node_src, middle_index + 1}};
-    }
-    else if (middle_node_src + 1 == middle_node_sink)
-    {
-        // cout << "Diag" << '\n';
-        return {{middle_node_src, middle_index}, {middle_node_sink, middle_index + 1}};
+        // Horz edge
+        return {{middle_node, col}, {middle_node, col + 1}};
     }
     else
     {
-        // cout << "Vertical" << '\n';
-        return {{middle_node_src, middle_index}, {middle_node_src + 1, middle_index}};
+        // Vertical edge
+        return {{middle_node, col}, {middle_node + 1, col}};
     }
 }
